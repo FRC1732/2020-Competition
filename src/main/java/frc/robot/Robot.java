2977afaf43f68fbad6e5b627fe71ea34dd788226
Matthew <duffym22@gmail.com>
2020-01-13 12:18:45 -0600

    Swapped direct set to use Scheduler instead (TBT)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Subsystems.Drivetrain;
 
 /**
@@ -94,7 +95,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    drivetrain.set(oi.getLeftJoystick(), oi.getRightJoystick());
+    CommandScheduler.getInstance().run();
   }
   
   /**
