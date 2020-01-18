33e8272b9739a062af76cc4604da4f769d4b77aa
stones30 <stones30@muhs.edu>
2020-01-18 11:50:56 -0600

    fixed things (changed double to int) and comments

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,7 @@
 
 package frc.robot;
 
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
  * numerical or boolean constants. This class should not be used for any other
@@ -18,37 +19,41 @@ package frc.robot;
  * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    public static final double CLIMBER_RIGHT_ID = 10;
-    public static final double CLIMBER_LEFT_ID = 35;
-    public static final double CLIMBER_DRIVE_ID = 17;
-    public static final double CLIMBING_SOLENOID_ID = 1;
+    //Declare all constants in the form: SUBSYSTEM_NAME_ID = x;
+
+    //Climber mapping
+    public static final int CLIMBER_RIGHT_ID = 10;
+    public static final int CLIMBER_LEFT_ID = 35;
+    public static final int CLIMBER_DRIVE_ID = 17;
+    public static final int CLIMBING_SOLENOID_ID = 1;
     
-    public static final double DRIVETRAIN_LEFTMASTER_ID = 32;
-    public static final double DRIVETRAIN_LEFT1_ID = 33;
-    public static final double DRIVETRAIN_LEFT2_ID = 34;
-    public static final double DRIVETRAIN_RIGHTMASTER_ID = 11;
-    public static final double DRIVETRAIN_RIGHT1_ID = 12; 
-    public static final double DRIVETRAIN_RIGHT2_ID = 13;
+    //Drivetrain mapping
+    public static final int DRIVETRAIN_LEFTMASTER_ID = 32;
+    public static final int DRIVETRAIN_LEFT1_ID = 33;
+    public static final int DRIVETRAIN_LEFT2_ID = 34;
+    public static final int DRIVETRAIN_RIGHTMASTER_ID = 11;
+    public static final int DRIVETRAIN_RIGHT1_ID = 12; 
+    public static final int DRIVETRAIN_RIGHT2_ID = 13;
     
-    public static final double SHOOTER_SHOOTER_ID = 9;
-    public static final double SHOOTER_ADJUSTMENT_SOLENOID_ID = 5;
-    public static final double SHOOTER_ROTATION_SOLENOID_ID = 6;
+    //Shooter mapping
+    public static final int SHOOTER_SHOOTER_ID = 9;
+    public static final int SHOOTER_ADJUSTMENT_SOLENOID_ID = 5;
+    public static final int SHOOTER_ROTATION_SOLENOID_ID = 6;
 
-    public static final double INDEXER_CELLHOLDER1_ID = 30;
-    public static final double INDEXER_CELLHOLDER2_ID = 28;
-    public static final double INDEXER_CONVEYER_ID = 15;
-    public static final double INDEXER_CELLGATE_ID = 3;
+    //Indexer mapping
+    public static final int INDEXER_CELLHOLDER1_ID = 30;
+    public static final int INDEXER_CELLHOLDER2_ID = 28;
+    public static final int INDEXER_CONVEYER_ID = 15;
+    public static final int INDEXER_CELLGATE_ID = 3;
 
-    public static final double INTAKE_INTAKEMOTOR_ID = 16;
-    public static final double INTAKE_INTAKESOLENOID_ID = 0;
+    //Intake mapping
+    public static final int INTAKE_INTAKEMOTOR_ID = 16;
+    public static final int INTAKE_INTAKESOLENOID_ID = 0;
 
-    public static final double CONTROLPANEL_MOTOR_ID = 14;
-    public static final double CONTROLPANEL_TRENCH_HARDSTOP_SOLENOID_ID = 4;
+    //ControlPanelManip mapping
+    public static final int CONTROLPANELMANIP_MOTOR_ID = 14;
+    public static final int CONTROLPANELMANIP_TRENCH_HARDSTOP_SOLENOID_ID = 4;
 
-    public static final double VISION_LIMELIGHT_ID = 31;
-
-
-
-
-
+    //Vision mapping
+    public static final int VISION_LIMELIGHT_ID = 31;
 }
