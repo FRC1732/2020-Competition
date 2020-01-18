d44e80c34ceeef9e63325f0ff94ac47dc6888fa7
Omar Saleh <salehmaram09@gmail.com>
2020-01-18 13:49:35 -0600

    Shooter Method

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+  /**
+   * Creates a new Shooter.
+   */
+  private CANSparkMax motor1 = new CANSparkMax(0, MotorType.kBrushless);
+  private CANSparkMax motor2 = new CANSparkMax(0, MotorType.kBrushless);
+ 
+  public Shooter() {
+   
+  }
+
+  public void set(double speed){
+    motor1.set(speed);
+    motor2.set(speed);
+  }
+
+  public double getShooter() {
+    motor1.get();
+    motor2.get();
+    //Add code to get speed
+  }
+
+  public boolean atSpeed() {
+    return false;
+  }
+  
+  public void shootBall() {
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
