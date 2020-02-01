eed6143d9289f6f4b02aa14cb61fd03cb0deaf52
Omar Saleh <salehmaram09@gmail.com>
2020-02-01 13:09:09 -0600

    Fixed Errors

--- src/main/java/frc/robot/commands/Autonomous/DriveForward.java
+++ src/main/java/frc/robot/commands/Autonomous/DriveForward.java
@@ -20,34 +20,11 @@ public class DriveForward extends SequentialCommandGroup {
   /**
    * Creates a new DriveForward.
    */
-  public DriveForward(final Drivetrain subsystem) {
+  public DriveForward(Drivetrain drivetrain) {
     //m_Drivetrain = subsystem;
-
+    addCommands(new DrivetrainOn(drivetrain), new WaitCommand(5), new DrivetrainOff(drivetrain) );
     
     // Add your commands in the super() call, e.g.
     // super(new FooCommand(), new BarCommand())
-  }
-  @Override
-  public void initialize() {
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    
-  }
-  public void timePeroid(){
-    addCommands(new WaitCommand(60));
-  }
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
   }
 }
