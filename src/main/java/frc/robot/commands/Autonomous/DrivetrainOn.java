f7f422a71a090e25b758e06d54085418ab918376
Omar Saleh <salehmaram09@gmail.com>
2020-02-01 11:53:15 -0600

    Drive Commands Updated

--- src/main/java/frc/robot/commands/Autonomous/DrivetrainOn.java
+++ src/main/java/frc/robot/commands/Autonomous/DrivetrainOn.java
@@ -7,47 +7,41 @@
 
 package frc.robot.commands.Autonomous;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.Subsystem;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
-public class DriveForward extends SequentialCommandGroup {
-  //private Drivetrain m_Drivetrain;
+public class DrivetrainOn extends CommandBase {
+  private Drivetrain drivetrain;
   /**
-   * Creates a new DriveForward.
+   * Creates a new DrivetrainOn.
    */
-  public DriveForward(final Drivetrain subsystem) {
-    //m_Drivetrain = subsystem;
-
-    
-    // Add your commands in the super() call, e.g.
-    // super(new FooCommand(), new BarCommand())
+  public DrivetrainOn(Drivetrain drivetrain) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drivetrain);
+    this.drivetrain = drivetrain;
   }
+  
+  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    drivetrain.set(0.5, 0.5);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+    //Execute not used
   }
-  public void timePeroid(){
-    addCommands(new WaitCommand(60));
-  }
+
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-
+    //Execute not used
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
