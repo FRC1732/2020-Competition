96007366778a075606914e0da94e30d5d22a0ca3
Omar Saleh <salehmaram09@gmail.com>
2020-02-08 13:42:32 -0600

    Fixed errors

---
+++ src/main/java/frc/robot/commands/GetColor.java
@@ -0,0 +1,61 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import com.revrobotics.ColorSensorV3;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ControlPanel;
+
+public class GetColor extends CommandBase {
+  /**
+   * Creates a new GetColor.
+   */
+  private ControlPanel controlpanel;
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  private final ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
+  private Color detectedColor = colorSensor.getColor();
+  private double IR = colorSensor.getIR();
+  
+  public GetColor(ControlPanel controlpanel) {
+    this.controlpanel = controlpanel;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(controlpanel);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("IR", IR);
+    int proximity = colorSensor.getProximity();
+
+    SmartDashboard.putNumber("Proximity", proximity);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
